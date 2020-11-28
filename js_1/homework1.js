4716cfd94872e9510c615a695e3e966c10372d80
Kozhurin Dmytro <dimakozhurin28@gmail.com>
2020-11-28 19:03:28 +0300

    js_first_homework

---
+++ js_1/homework1.js
@@ -0,0 +1,52 @@
+// first
+
+// let go = true;
+//
+//
+// while (go) {
+//     let result = confirm('twist?');
+//     if (result === true) go = false
+//
+// }
+
+// second
+
+// let mass = Array.from(Array(10).keys()).map((item, i, arr) => {
+//    return i *= i * i;
+//
+// })
+// console.log(mass)
+
+
+//third
+
+// let n = prompt('Введите число');
+// let sum = 0;
+//
+// for (let i = 1; i <= n; i += 3) {
+//     sum += i
+// }
+// console.log(sum)
+
+//fourth
+
+let a = '.#';
+let horizontal = +prompt('Длина горизонтали');
+let vertical = +prompt('Длина вертикали');
+let board = '';
+let line = '';
+
+for (let v = vertical; v > 0; v--) {
+    line = '';
+    for (let h = horizontal; h > 0; h--) {
+        line += a;
+
+    }
+    if (v % 2 === 0) {
+        line = line.split('').reverse().join('');
+
+    }
+    board += line + '\n';
+}
+
+console.log(board)
